8d462007c5cca9ec12720d4214396c2a99edf1b7
pawellak <pawellak@gmail.com>
2025-10-07 10:26:56 +0200

    v1.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1142544610"
+    serviceWorkerVersion: "1631846051"
   }
 });
